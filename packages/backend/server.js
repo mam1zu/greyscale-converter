50dc0f0690f8d04b569212960f8bfdd789499844
mam1zu <Mamizu0312@gmail.com>
2023-06-03 23:50:15 +0900

    implement handlesubmit

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const sharp = require('sharp');
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -12,6 +13,10 @@ app.get('/', (req, res) => {
     res.status(200).send("API SERVER");
 });
 
-app.get('/api/imageconvert', (req, res) => {
-    
+app.get('/api/convert/jpeg', (req, res) => {
+    //jpegファイルを白黒化してjpegファイルとして返す
+});
+
+app.get('/api/convert/png', (req, res) => {
+    //pngファイルを白黒化してpngファイルとして返す
 });
--- packages/frontend/src/components/ImageInput.js
+++ packages/frontend/src/components/ImageInput.js
@@ -1,4 +1,24 @@
 export const ImageInput = () => {
+    const [img, setImg] = useState(undefined);
+    const [convertedImg, setConvertedImg] = useState(undefined);
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await fetch("/api/convert/jpeg", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "image/jpeg"
+            },
+            body: img
+        })
+        .then(res => {
+            if(res.status() == 200) {
+                return <img src={convertedImg}></img>
+            }
+            else {
+                return null;
+            }
+        })
+    }
     return(
         <div>
             <form>
