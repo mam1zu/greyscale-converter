7f73b07fd9fbf8f59521c1de2fa957ea2873fa86
mam1zu <Mamizu0312@gmail.com>
2023-06-04 16:18:17 +0900

    途中経過

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,22 +1,47 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const sharp = require('sharp');
+const fs = require('fs');
+const { time } = require('console');
+const path = require('path');
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
     res.status(200).send("API SERVER");
 });
 
-app.get('/api/convert/jpeg', (req, res) => {
-    //jpegファイルを白黒化してjpegファイルとして返す
+app.post('/api/convert/jpeg', (req, res) => {
+
+    try {
+        const fpath = path.join(__dirname, "temp", getTimeNow() + ".jpeg");
+        console.log(fpath);
+        console.log(req);
+        //受け取ったjpegファイルを白黒化して一時的に保存, 名前はなんかいい感じのフォーマットを考える
+        fs.writeFileSync(fpath, req.body.image);
+        res.status(200).sendFile(fpath);
+        //送信し終わったのでファイルは消す
+    } catch (err) {
+        console.error(err);
+        res.status(422).send("Invalid parameters");
+    }
 });
 
 app.get('/api/convert/png', (req, res) => {
     //pngファイルを白黒化してpngファイルとして返す
 });
+
+app.use((err, req, res, next) => {
+    res.status(500).send('Internal Server Error');
+    console.log("500 Internal Server Error");
+    console.log(err);
+})
+
+app.listen(8000, () => {
+    console.log("Login API Server Start \n Listening on port: 8000");
+});
+
+function getTimeNow() {
+    return new Date().getTime();
+}
--- packages/frontend/src/components/ImageInput.js
+++ packages/frontend/src/components/ImageInput.js
@@ -1,28 +1,40 @@
+import { useState } from 'react';
+
 export const ImageInput = () => {
-    const [img, setImg] = useState(undefined);
-    const [convertedImg, setConvertedImg] = useState(undefined);
+
+    const [img, setImg] = useState();
+    const [convertedImg, setConvertedImg] = useState();
+
     const handleSubmit = async (event) => {
+
         event.preventDefault();
+        const img_blob = new Blob(img, {type: "image/jpeg"});
+        console.log(img_blob);
         await fetch("/api/convert/jpeg", {
             method: 'POST',
             headers: {
-                "Content-Type": "image/jpeg"
+                "Content-Type": 'application/octet-stream',
             },
-            body: img
+            body: img_blob
+
         })
         .then(res => {
-            if(res.status() == 200) {
-                return <img src={convertedImg}></img>
+            console.log(res);
+            if(res.status === 200) {
+                const img_mono = res.blob();
+
+                setConvertedImg(img_mono);
+                return <img src={convertedImg}></img>;
             }
             else {
-                return null;
+                return <p>なんか失敗</p>;
             }
         })
     }
     return(
         <div>
-            <form>
-                <input type="file" name="image" accept="image/jpeg, image/png"></input>
+            <form onSubmit={handleSubmit}>
+                <input type="file" name="image" accept="image/jpeg, image/png" onChange={(event) => {event.preventDefault(); setImg(event.target.files);}}></input>
                 <input type="submit"></input>
             </form>
         </div>
