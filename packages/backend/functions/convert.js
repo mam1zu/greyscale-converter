4f91fdf4ddc2903e55f7bf981f34d1510afd6e72
mam1zu <Mamizu0312@gmail.com>
2023-06-07 00:08:08 +0900

    implement convert.js

--- packages/backend/functions/convert.js
+++ packages/backend/functions/convert.js
@@ -1,7 +1,13 @@
 const sharp = require('sharp');
 
 const convert = (type, buf) => {
-
+    const monoimage = sharp(buf);
+    if(type == "image/jpeg") {
+        return monoimage.greyscale().jpeg();
+    }
+    if(type == "image/png") {
+        return monoimage.greyscale().png();
+    }
 }
 
 module.exports = convert;
